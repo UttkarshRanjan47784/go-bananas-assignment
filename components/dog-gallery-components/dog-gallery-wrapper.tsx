df7f6a4a648be3595817d3009fd5473a59f50fab
UttkarshRanjan47784 <utr.1206@gmail.com>
2024-06-05 14:24:50 +0530

    Minor Bug Fix

--- components/dog-gallery-components/dog-gallery-wrapper.tsx
+++ components/dog-gallery-components/dog-gallery-wrapper.tsx
@@ -17,22 +17,24 @@ export default async function DogGalleryWrapper() {
   console.log(`Dogs dogs`);
   console.log(doggyPics);
 
-  const renderDogPics = doggyPics.data.map((item: doggyPicInfo) => {
-    return (
-      <div className="min-h-72 relative">
-        <Image
-          alt="Doggo Pic"
-          src={item.url}
-          quality={100}
-          fill
-          sizes="(min-width: 808px) 50vw, 100vw"
-          style={{
-            objectFit: "cover",
-          }}
-        />
-      </div>
-    );
-  });
+  const renderDogPics = doggyPics.data.map(
+    (item: doggyPicInfo, index: number) => {
+      return (
+        <div key={`${index}${Math.random()}`} className="min-h-72 relative">
+          <Image
+            alt="Doggo Pic"
+            src={item.url}
+            quality={100}
+            fill
+            sizes="(min-width: 808px) 50vw, 100vw"
+            style={{
+              objectFit: "cover",
+            }}
+          />
+        </div>
+      );
+    }
+  );
 
   return (
     <div className="grow w-full p-5 grid grid-cols-1 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-3">
